ff00b2028a634c8463d89854c90be1a1135da0d8
Andrii Shlapachenko <andriishlap@gmail.com>
2024-02-24 13:51:45 +0000

    comments added

--- test/Login page tests/test1.1 - parametrized login with set of users.js
+++ test/Login page tests/test1.1 - parametrized login with set of users.js
@@ -2,6 +2,8 @@ const { Builder, By, until } = require("selenium-webdriver");
 const assert = require("assert");
 
 describe ('Login page tests', function (){
+
+  //set of user logins
   const tests =[
     {userName:"standard_user"},
     {userName:"problem_user"},
@@ -10,16 +12,29 @@ describe ('Login page tests', function (){
     {userName:"visual_user"}
   ];
 
+  // forEach userName value a separate test will run.
   tests.forEach(({userName}) => {
     it(`Succsess parametrized login with ${userName} login`, async function (){
+
+      //run browser chrome
       let driver = await new Builder().forBrowser("chrome").build();
+
+      //navigate to website
       await driver.get("https://www.saucedemo.com/");
+
+      //find and input to username and pass fields
       await driver.findElement(By.id("user-name")).sendKeys(userName);
       await driver.findElement(By.id("password")).sendKeys("secret_sauce");
+
+      //click login button
       await driver.findElement(By.id("login-button")).click();
+
+      //assertion to compare expected url after login and actual
       const EXPECTED_URL = "https://www.saucedemo.com/inventory.html";
       const ACTUAL_URL = await driver.getCurrentUrl();
       assert.strictEqual(ACTUAL_URL, EXPECTED_URL);
+
+      //close the browser
       await driver.quit();
     })
   })
